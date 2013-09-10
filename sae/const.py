63ab9432fef1b54bbd83c84e9d8cfaa9363a71d3
T.G <farmer1992@gmail.com>
2013-09-11 00:58:20 +0800

    add sae const and kvdb

---
+++ sae/const.py
@@ -0,0 +1,10 @@
+
+import os
+
+MYSQL_USER = 'adminiuU2ltN'
+MYSQL_PASS = 'edIel716v8ge'
+
+MYSQL_DB = 'taobb'
+
+MYSQL_HOST = os.environ['OPENSHIFT_MYSQL_DB_HOST']
+MYSQL_PORT = os.environ['OPENSHIFT_MYSQL_DB_PORT']
