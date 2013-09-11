89bafb0a863512882ba872a3e820e6ad54be2f44
T.G <farmer1992@gmail.com>
2013-09-11 20:36:59 +0800

    data using env

--- sae/const.py
+++ sae/const.py
@@ -1,10 +1,9 @@
-
 import os
-
-MYSQL_USER = 'adminiuU2ltN'
-MYSQL_PASS = 'edIel716v8ge'
-
-MYSQL_DB = 'taobb'
 
 MYSQL_HOST = os.environ['OPENSHIFT_MYSQL_DB_HOST']
 MYSQL_PORT = os.environ['OPENSHIFT_MYSQL_DB_PORT']
+
+MYSQL_USER = os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
+MYSQL_PASS = os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+
+MYSQL_DB = 'taobb'
